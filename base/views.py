9ea4555a2a671d3057c8d78ac3153bda7b97a47f
Jacob Allen <enoua5@gmail.com>
2022-11-17 19:27:25 -0700

    Show top players

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 @login_required
 def index(req):
-    return render(req, 'base/index.html')
+    top_players = User.objects.all().order_by('userdata__elo_rating')[:5]
+
+    ctx = {
+        'top_players': top_players
+    }
+
+    return render(req, 'base/index.html', ctx)
