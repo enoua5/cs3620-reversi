a50d3ca4096fd816f246a5668135171b5ff5cba9
Jacob Allen <enoua5@gmail.com>
2022-12-01 22:12:01 -0700

    Add open_for_challenge and bio_text fields to UserData

--- users/models.py
+++ users/models.py
@@ -7,4 +7,7 @@ class UserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     elo_rating = models.IntegerField(default=1000)
+    open_for_challenge = models.BooleanField(default=True)
+    bio_text = models.CharField(max_length=2000, default="Hi! I'm new here!")
+
 
